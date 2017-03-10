262fbd51434f545937cdcb89f35da0433505967e
Muhammad Asif <mughal.asif02@gmail.com>
2017-03-10 10:10:24 -0800

    login

---
+++ ComplainCenter/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ComplainCenter.Controllers
+{
+   
+    public class AccountController : Controller
+    {
+        ComplainCenterEntities db = new ComplainCenterEntities();
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string email, string password)
+        {
+            var user = db.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
+            if(user == null)
+            {
+                ViewBag.Error = "Invalid Email or Password....";
+                return View("Index");
+               
+            }
+
+            else
+            {
+                Session["email"] = user.Email;
+                Session["uid"] = user.Id;
+                Session["name"] = user.Name;
+                Session["ut"] = user.UserType.Name;
+
+                return RedirectToAction("Index", "Home");
+               
+            }
+        }
+
+    }
+}
